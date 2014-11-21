6e0f653f5ba57a7963c06160e14cae53fa47e3e7
jokesterfr <main@jokester.fr>
2014-11-21 19:17:13 +0100

    Update syntax and fix data-localize listener
    Watched element was'nt watched, only elements sibling were... Fixed

--- localizer.js
+++ localizer.js
@@ -93,136 +93,134 @@
 		// Export can be defined once (singleton)
 		var that = this;
 		window.localizer = this;
-		window.localize = function (key, data) {
-			return that.localize(key, data);
-		};
+		window.localize = this.localize.bind(this);
 	};
 
 	/**
 	 * Localizer options
 	 */
-	Localizer.prototype.options = {
-		// Where the pre-built javascript files will be retrieved from
-		localePath: 'locales/{locale}/i18n.js',
+	Localizer.prototype = {
+		options: {
+			// Where the pre-built javascript files will be retrieved from
+			localePath: 'locales/{locale}/i18n.js',
 
-		// The default locale to be used
-		defaultLocale: 'en_US',
+			// The default locale to be used
+			defaultLocale: 'en_US',
 
-		// Where the script is inserted in the page
-		scriptAnchor: 'i18n-src',
-	};
+			// Where the script is inserted in the page
+			scriptAnchor: 'i18n-src',
+		},
 
-	/**
-	 * Load new language script if not the same
-	 * and create and dispatch the event.
-	 * @public
-	 * @param {String} locale
-	 * @emits {Object} localeChange - "en_US" for example
-	 * @return none
-	 */
-	Localizer.prototype.setLocale = function (locale) {
-		if (!locale && this.locale) throw new Error('no locale given');
-		else if (locale === this.locale) return;
-		this.locale = locale;
-		
-		// Dispatch event on document for listeners
-		var evt = new CustomEvent('localeChange', { detail: locale });
-		document.dispatchEvent(evt);
+		/**
+		 * Load new language script if not the same
+		 * and create and dispatch the event.
+		 * @public
+		 * @param {String} locale
+		 * @emits {Object} localeChange - "en_US" for example
+		 * @return none
+		 */
+		setLocale: function (locale) {
+			if (!locale && this.locale) throw new Error('no locale given');
+			else if (locale === this.locale) return;
+			this.locale = locale;
+			
+			// Dispatch event on document for listeners
+			var evt = new CustomEvent('localeChange', { detail: locale });
+			document.dispatchEvent(evt);
 
-		var that = this;
-		var path = this.options.localePath.replace(/\{locale\}/, locale);
-		insertScript(path, this.options.scriptAnchor, function () {
-			console.log('language changed to', that.locale);
-			// Apply change to registered elements
-			that.registeredElements.forEach(function (elt) {
-				that.localize(elt);
-			});
-		});
-		return;
-	};
+			// Insert language file and apply changes to all registered elements
+			var path = this.options.localePath.replace(/\{locale\}/, locale);
+			insertScript(path, this.options.scriptAnchor, function () {
+				console.log('language changed to', this.locale);		
+				this.registeredElements.forEach(this.localize.bind(this));
+			}.bind(this));
+			return;
+		},
 
-	/**
-	 * Simple getter for current locale.
-	 * @return {String} locale
-	 */
-	Localizer.prototype.getLocale = function () {
-		return this.locale;
-	};
+		/**
+		 * Simple getter for current locale.
+		 * @return {String} locale
+		 */
+		getLocale: function () {
+			return this.locale;
+		},
 
-	/**
-	 * Localizer the string in accordance with given data,
-	 * if key is an HTML element, this handle it properly.
-	 * @param {String or HTMLElement} key
-	 * @param {Object} data
-	 * @return {String}
-	 */
-	Localizer.prototype.localize = function (key, data) {
-		if (!key) return;
+		/**
+		 * Localizer the string in accordance with given data,
+		 * if key is an HTML element, this handle it properly.
+		 * @param {String or HTMLElement} key
+		 * @param {Object} data
+		 * @return {String}
+		 */
+		localize: function (key, data) {
+			if (!key) return;
 
-		// String case
-		// -----------
-		if (!(key instanceof HTMLElement)) {
-			var func = window.i18n && window.i18n[key];
-			if (func) {
-				return func(data);
+			// String case
+			// -----------
+			if (!(key instanceof HTMLElement)) {
+				var func = window.i18n && window.i18n[key];
+				if (func) {
+					return func(data);
+				}
+				return key;
 			}
-			return key;
-		}
-		
-		// HTMLElement case
-		// ----------------
+			
+			// HTMLElement case
+			// ----------------
 
-		// Register it
-		var elt = key;
-		if (this.registeredElements.indexOf(elt) === -1) {
-			this.registeredElements.push(elt);
-		}
+			// Register it
+			var elt = key;
+			if (this.registeredElements.indexOf(elt) === -1) {
+				this.registeredElements.push(elt);
+			}
 
-		// Apply localize change
-		var nodes = elt.querySelectorAll('[data-localize]');
-		Array.prototype.forEach.call(nodes, function (node) {
-			var dataset = node.dataset,
-				data = {},
-				attr = dataset && dataset.localize,
-				func = window.i18n && window.i18n[attr || node.innerHTML],
-				key;
-			if (func) {
-				if (!attr) {
+			// Apply localize change
+			if (elt.hasAttribute('data-localize')) elt = elt.parentNode;
+			var nodes = elt.querySelectorAll('[data-localize]');
+			Array.prototype.forEach.call(nodes, function (node) {
+				var dataset = node.dataset,
+					data = {},
+					attr = dataset && dataset.localize,
+					func = window.i18n && window.i18n[attr || node.innerHTML],
+					key;
+				if (func) {
+					if (!attr) {
+						node.setAttribute('data-localize', node.textContent);
+					}
+					for (key in dataset) {
+						// Data is guessed on the fly with the dataset
+						if (dataset.hasOwnProperty(key) && key !== 'localize') {
+							data[key] = escapeHTML(dataset[key]);
+						}
+					}
+					node.innerHTML = func(data);
+				} else if (attr) {
+					node.textContent = attr;
+				} else {
 					node.setAttribute('data-localize', node.textContent);
 				}
-				for (key in dataset) {
-					// Data is guessed on the fly with the dataset
-					if (dataset.hasOwnProperty(key) && key !== 'localize') {
-						data[key] = escapeHTML(dataset[key]);
-					}
-				}
-				node.innerHTML = func(data);
-			} else if (attr) {
-				node.textContent = attr;
-			} else {
-				node.setAttribute('data-localize', node.textContent);
-			}
-		});
-		return;
-	};
+			});
+			return;
+		},
 
-	/**
-	 * Add a listener on localeChange event.
-	 * We do use here the native *addEventListener*
-	 * mecanism to register listeners.
-	 * @param {String} eventName
-	 * @param {Function} listener
-	 * @return none
-	 */
-	Localizer.prototype.on = function(eventName, listener) {
-		listener = typeof eventName === 'function' ? eventName : listener;
-		if (!listener) throw new Error('listener cannot be null');
-		eventName = 'localeChange'; // only one for now
-		document.addEventListener(eventName, function (evt) {
-			return listener(evt.detail);
-		});
+		/**
+		 * Add a listener on localeChange event.
+		 * We do use here the native *addEventListener*
+		 * mecanism to register listeners.
+		 * @param {String} eventName
+		 * @param {Function} listener
+		 * @return none
+		 */
+		on: function(eventName, listener) {
+			listener = typeof eventName === 'function' ? eventName : listener;
+			if (!listener) throw new Error('listener cannot be null');
+			eventName = 'localeChange'; // only one for now
+			document.addEventListener(eventName, function (evt) {
+				return listener(evt.detail);
+			});
+		}
 	}
-	
+
 	/**
 	 * @alias Localizer.prototype.on
 	 */
